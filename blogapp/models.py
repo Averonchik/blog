2c465324876e20a066054bebb4c98ffc379268ba
Averonchik <yurgin.rodion@gmail.com>
2018-01-01 22:36:04 +0800

    add & little edit models, add form post

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,12 +1,24 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class ProfileUser(models.Model):
+    user = models.ForeignKey(User, related_name='author')
+    subscribers = models.ManyToManyField('self', related_name='subs', symmetrical=False)
+    read_posts = models.ManyToManyField('Post', related_name='read')
+
+    def __str__(self):
+        return str(self.user)
+
+
+User.profile = property(lambda u: ProfileUser.objects.get_or_create(user=u)[0])
+
+
 class Post(models.Model):
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User')
-    date = models.DateTimeField(default=timezone.now)
+    date = models.DateTimeField(auto_now=True)
     entry = models.TextField()
 
     def __str__(self):
